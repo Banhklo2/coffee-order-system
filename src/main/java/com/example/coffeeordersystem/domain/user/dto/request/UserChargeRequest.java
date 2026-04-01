0fb8478a5e6693e68b10ebe802f3727c62dd5681
HYOKYUNG BANG <101386329+Banhklo2@users.noreply.github.com>
2026-04-01 20:16:08 +0900

    refactor: Request DTO 어노테이션 구조 일관성 맞춤

--- src/main/java/com/example/coffeeordersystem/domain/user/dto/request/UserChargeRequest.java
+++ src/main/java/com/example/coffeeordersystem/domain/user/dto/request/UserChargeRequest.java
@@ -1,10 +1,14 @@
 package com.example.coffeeordersystem.domain.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserChargeRequest {
 
     private BigDecimal amount;
